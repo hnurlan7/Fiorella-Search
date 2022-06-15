19711e48388a554f70b9032b4fc7bdf41bbe3467
hnurlan7 <tu6f7xcr3@code.edu.az>
2022-06-15 13:37:55 +0400

    Featured: Fiorella Search

---
+++ Fiorello-Search/Migrations/20220615093343_FiorellaSearch.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace fiorello.Migrations
+{
+    public partial class FiorellaSearch : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
